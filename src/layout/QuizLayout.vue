04f3ef451e74de78c4235157e600b855ae79e7bb
Rishabh-Singhvi <rishabhsinghvi111@gmail.com>
2020-03-03 14:04:42 +0530

    quiz buttons

--- src/layout/QuizLayout.vue
+++ src/layout/QuizLayout.vue
@@ -6,7 +6,12 @@
       title="Argon"
     >
       <template slot="links">
-        <sidebar-item v-for="question in quizObj.questions" v-bind:key="question.number" :link="{name: 'Question '+question.number, icon: 'ni ni-planet text-blue', path: '/quiz/'+quizId+'/'+question.number}"/>
+       <div  v-for="question in quizObj.questions" v-bind:key="question.number">
+        <sidebar-item v-if="question.selected != '' && !question.marked" :link="{name: 'Question '+question.number, icon: 'ni ni-check-bold', path: '/quiz/'+quizId+'/'+question.number}"/>
+        <sidebar-item v-if="question.selected == '' && !question.marked" :link="{name: 'Question '+question.number, icon: 'ni ni-planet text-blue', path: '/quiz/'+quizId+'/'+question.number}"/>
+        <sidebar-item v-if="question.selected == '' && question.marked" :link="{name: 'Question '+question.number, icon: 'ni ni-active-40', path: '/quiz/'+quizId+'/'+question.number}"/>
+        <sidebar-item v-if="question.selected != '' && question.marked" :link="{name: 'Question '+question.number, icon: 'ni ni-glasses-2', path: '/quiz/'+quizId+'/'+question.number}"/>
+       </div>
       </template>
     </side-bar>
     <div class="main-content" :data="sidebarBackground">
@@ -15,9 +20,11 @@
       <div @click="toggleSidebar">
         <fade-transition :duration="200" origin="center top" mode="out-in">
           <!-- your content here -->
-          <router-view :quizObj="quizObj" ></router-view>
+          <router-view :quizId="quizId" :quizObj="quizObj" ></router-view>
         </fade-transition>
       </div>
+        
+            
     </div>
   </div>
 </template>
@@ -36,7 +43,8 @@
       return {
         sidebarBackground: 'vue', //vue|blue|orange|green|red|primary
         quizObj:{},
-        quizId:Number
+        quizId:Number,
+        // onclk:false
       };
     },
     methods: {
@@ -54,37 +62,41 @@
       this.quizObj = {
         'name':"First Quiz",
         'questions':[
-          {
+          { 
+            'marked':false,
             'number':1,
-            'text':"Text Goes Here",
+            'text':"Text Goes Here 1",
             'options':[
               'ans11','ans22','ans3','ans4'
             ],
-            'correct':0
+            'selected':''
           },
           {
+            'marked':false,
             'number':2,
-            'text':"Text Goes Here",
+            'text':"Text Goes Here 2",
             'options':[
               'ans1','ans2','ans3','ans4'
             ],
-            'correct':0
+            'selected':''
           },
           {
+            'marked':false,
             'number':3,
             'text':"Text Goes Here",
             'options':[
               'ans1','ans2','ans3','ans4'
             ],
-            'correct':0
+            'selected':''
           },
           {
+            'marked':false,
             'number':4,
             'text':"Text Goes Here",
             'options':[
               'ans1','ans2','ans3','ans4'
             ],
-            'correct':0
+            'selected':''
           },
           
         ]
