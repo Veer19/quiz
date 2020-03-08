c0e5725013a6ed8c46775e99fec668cee6076efd
Veer Singh <veer.vs19@gmail.com>
2020-03-08 09:49:21 +0530

    Started Create Quiz

--- .history/src/router_20200227131153.js
+++ .history/src/router_20200227131153.js
@@ -22,14 +22,6 @@ export default new Router({
           component: () => import(/* webpackChunkName: "demo" */ './views/Dashboard.vue')
         },
         {
-          path: '/create',
-          name: 'create-quiz',
-          // route level code-splitting
-          // this generates a separate chunk (about.[hash].js) for this route
-          // which is lazy-loaded when the route is visited.
-          component: () => import(/* webpackChunkName: "demo" */ './views/CreateQuiz.vue')
-        },
-        {
           path: '/icons',
           name: 'icons',
           component: () => import(/* webpackChunkName: "demo" */ './views/Icons.vue')
